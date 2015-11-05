3fd7971873862fc0cddb1492f75361d1b2854c4b
HJin_me <hjin.me@gmail.com>
2015-11-05 15:41:04 +0800

    update

--- bin/hulix.js
+++ bin/hulix.js
@@ -1,23 +1,23 @@
 #!/usr/bin/env node
 
-var path = require("path");
-var url = require("url");
-var fs = require("fs");
+var path = require('path');
+var url = require('url');
+var fs = require('fs');
 
 // Local version replaces global one
 try {
-    var localHulix = require.resolve(path.join(process.cwd(), "node_modules", "hulix", "bin", "hulix.js"));
+    var localHulix = require.resolve(path.join(process.cwd(), 'node_modules', 'hulix', 'bin', 'hulix.js'));
     if (__filename !== localHulix) {
         return require(localHulix);
     }
 } catch (e) {
 }
 
-var optimist = require("optimist")
-    .usage("hulix " + require("../package.json").version + "\n" +
-    "Usage: https://webpack.github.io/docs/cli.html");
+var optimist = require('optimist')
+    .usage('hulix ' + require('../package.json').version + '\n' +
+        'Usage: https://webpack.github.io/docs/cli.html');
 var webpack = require('webpack');
-var webpackDevServer = require('webpack-dev-server');
+var WebpackDevServer = require('webpack-dev-server');
 var argv = require('minimist')(process.argv.slice(2));
 var action = 'help';
 if (argv._.indexOf('dev') > -1) {
@@ -47,63 +47,63 @@ function buildCode() {
         watch: false,
         cache: true
     };
-    var options = require("webpack/bin/convert-argv")(optimist, argv);
+    var options = require('webpack/bin/convert-argv')(optimist, argv);
     var firstOptions = Array.isArray(options) ? options[0] : options;
 
     var outputOptions = Object.create(options.stats || firstOptions.stats || {});
-    if (typeof outputOptions.context === "undefined") {
+    if (typeof outputOptions.context === 'undefined') {
         outputOptions.context = firstOptions.context;
     }
-    if (typeof outputOptions.colors === "undefined") {
-        outputOptions.colors = require("supports-color");
+    if (typeof outputOptions.colors === 'undefined') {
+        outputOptions.colors = require('supports-color');
     }
     if (!outputOptions.json) {
-        if (typeof outputOptions.cached === "undefined")
+        if (typeof outputOptions.cached === 'undefined')
             outputOptions.cached = false;
-        if (typeof outputOptions.cachedAssets === "undefined")
+        if (typeof outputOptions.cachedAssets === 'undefined')
             outputOptions.cachedAssets = false;
 
-        ifArg("display-chunks", function (bool) {
+        ifArg('display-chunks', function (bool) {
             outputOptions.modules = !bool;
             outputOptions.chunks = bool;
         });
 
-        ifArg("display-reasons", function (bool) {
+        ifArg('display-reasons', function (bool) {
             outputOptions.reasons = bool;
         });
 
-        ifArg("display-error-details", function (bool) {
+        ifArg('display-error-details', function (bool) {
             outputOptions.errorDetails = bool;
         });
 
-        ifArg("display-origins", function (bool) {
+        ifArg('display-origins', function (bool) {
             outputOptions.chunkOrigins = bool;
         });
 
-        ifArg("display-cached", function (bool) {
+        ifArg('display-cached', function (bool) {
             if (bool)
                 outputOptions.cached = true;
         });
 
-        ifArg("display-cached-assets", function (bool) {
+        ifArg('display-cached-assets', function (bool) {
             if (bool)
                 outputOptions.cachedAssets = true;
         });
 
-        if (!outputOptions.exclude && !argv["display-modules"])
-            outputOptions.exclude = ["node_modules", "bower_components", "jam", "components"];
+        if (!outputOptions.exclude && !argv['display-modules'])
+            outputOptions.exclude = ['node_modules', 'bower_components', 'jam', 'components'];
     } else {
-        if (typeof outputOptions.chunks === "undefined")
+        if (typeof outputOptions.chunks === 'undefined')
             outputOptions.chunks = true;
-        if (typeof outputOptions.modules === "undefined")
+        if (typeof outputOptions.modules === 'undefined')
             outputOptions.modules = true;
-        if (typeof outputOptions.chunkModules === "undefined")
+        if (typeof outputOptions.chunkModules === 'undefined')
             outputOptions.chunkModules = true;
-        if (typeof outputOptions.reasons === "undefined")
+        if (typeof outputOptions.reasons === 'undefined')
             outputOptions.reasons = true;
-        if (typeof outputOptions.cached === "undefined")
+        if (typeof outputOptions.cached === 'undefined')
             outputOptions.cached = true;
-        if (typeof outputOptions.cachedAssets === "undefined")
+        if (typeof outputOptions.cachedAssets === 'undefined')
             outputOptions.cachedAssets = true;
     }
 
@@ -122,7 +122,7 @@ function buildCode() {
             console.error(err.stack || err);
             if (err.details) console.error(err.details);
             if (!options.watch) {
-                process.on("exit", function () {
+                process.on('exit', function () {
                     process.exit(1);
                 });
             }
@@ -130,21 +130,164 @@ function buildCode() {
         }
         if (stats.hash !== lastHash) {
             lastHash = stats.hash;
-            process.stdout.write(stats.toString(outputOptions) + "\n");
+            process.stdout.write(stats.toString(outputOptions) + '\n');
         }
     }
 }
 
 function devServer() {
-    var BUILD = false;
-    var TEST = false;
-    var config = require('../lib/webpack.make.js')({
-        TEST: TEST,
-        BUILD: BUILD
+    var argv = require('minimist')(process.argv.slice(3));
+    argv.config = path.join(__dirname, '..', 'lib', 'webpack.config.js');
+    argv['history-api-fallback'] = true;
+    argv['inline'] = true;
+    argv['progress'] = true;
+    argv.cache = true;
+    argv.info = true;
+    if (!argv.port) {
+        argv.port = 8080;
+    }
+    if (!argv.host) {
+        argv.host = 'localhost';
+    }
+
+    var wpOpt = require('webpack/bin/convert-argv')(optimist, argv, {outputFilename: '/bundle.js'});
+    var firstWpOpt = Array.isArray(wpOpt) ? wpOpt[0] : wpOpt;
+    var options = wpOpt.devServer || firstWpOpt.devServer || {};
+    // load custom backend
+    var customBackEndConfig = path.join(process.cwd(), 'proxy.config.js');
+    var fd = fs.statSync(customBackEndConfig);
+    if (fd.isFile()) {
+        var customBackend = require(customBackEndConfig);
+        if (customBackend.proxy) {
+            options.proxy = customBackend.proxy
+        }
+        if (customBackend.host) {
+            options.host = customBackend.host
+        }
+        if (customBackend.port) {
+            options.port = customBackend.port
+        }
+    }
+    if (argv.host !== 'localhost' || !options.host) {
+        options.host = argv.host;
+    }
+    if (argv.port !== 8080 || !options.port) {
+        options.port = argv.port;
+    }
+    if (!options.publicPath) {
+        options.publicPath = firstWpOpt.output && firstWpOpt.output.publicPath || "";
+        if (!/^(https?:)?\/\//.test(options.publicPath) && options.publicPath[0] !== '/')
+            options.publicPath = '/' + options.publicPath;
+    }
+
+    if (!options.outputPath) {
+        options.outputPath = '/';
+    }
+    if (!options.filename) {
+        options.filename = firstWpOpt.output && firstWpOpt.output.filename;
+    }
+    [].concat(wpOpt).forEach(function (wpOpt) {
+        wpOpt.output.path = '/';
     });
-    var compiler = webpack(config);
-    var server = new webpackDevServer(compiler, {});
-    server.listen(8080);
+
+    if (!options.watchOptions) {
+        options.watchOptions = firstWpOpt.watchOptions;
+    }
+    if (!options.watchDelay && !options.watchOptions) // TODO remove in next major version
+    {
+        options.watchDelay = firstWpOpt.watchDelay;
+    }
+
+    if (!options.hot) {
+        options.hot = argv['hot'];
+    }
+
+    if (argv['content-base']) {
+        options.contentBase = argv['content-base'];
+        if (/^[0-9]$/.test(options.contentBase))
+            options.contentBase = +options.contentBase;
+        else if (!/^(https?:)?\/\//.test(options.contentBase))
+            options.contentBase = path.resolve(options.contentBase);
+    } else if (argv['content-base-target']) {
+        options.contentBase = {target: argv['content-base-target']};
+    } else if (!options.contentBase) {
+        options.contentBase = process.cwd();
+    }
+
+    if (!options.stats) {
+        options.stats = {
+            cached: false,
+            cachedAssets: false
+        };
+    }
+
+    if (typeof options.stats === 'object' && typeof options.stats.colors === 'undefined')
+        options.stats.colors = require('supports-color');
+
+    if (argv['lazy'])
+        options.lazy = true;
+
+    if (!argv['info'])
+        options.noInfo = true;
+
+    if (argv['quiet'])
+        options.quiet = true;
+
+    if (argv['https'])
+        options.https = true;
+
+    if (argv['cert'])
+        options.cert = fs.readFileSync(path.resolve(argv['cert']));
+
+    if (argv['key'])
+        options.key = fs.readFileSync(path.resolve(argv['key']));
+
+    if (argv['cacert'])
+        options.cacert = fs.readFileSync(path.resolve(argv['cacert']));
+
+    if (argv['inline'])
+        options.inline = true;
+
+    if (argv['history-api-fallback'])
+        options.historyApiFallback = true;
+
+    if (argv['compress'])
+        options.compress = true;
+
+    var protocol = options.https ? 'https' : 'http';
+
+    if (options.inline) {
+        var devClient = [require.resolve('webpack-dev-server/client') + '?' + protocol + '://' + options.host + ':' + options.port];
+
+        if (options.hot)
+            devClient.push('webpack/hot/dev-server');
+        [].concat(wpOpt).forEach(function (wpOpt) {
+            if (typeof wpOpt.entry === 'object' && !Array.isArray(wpOpt.entry)) {
+                Object.keys(wpOpt.entry).forEach(function (key) {
+                    wpOpt.entry[key] = devClient.concat(wpOpt.entry[key]);
+                });
+            } else {
+                wpOpt.entry = devClient.concat(wpOpt.entry);
+            }
+        });
+    }
+    var Server = require('webpack-dev-server');
+
+    new Server(webpack(wpOpt), options).listen(options.port, options.host, function (err) {
+        if (err) throw err;
+        if (options.inline)
+            console.log(protocol + '://' + options.host + ':' + options.port + '/');
+        else
+            console.log(protocol + '://' + options.host + ':' + options.port + '/webpack-dev-server/');
+        console.log('webpack result is served from ' + options.publicPath);
+        if (typeof options.contentBase === 'object')
+            console.log('requests are proxied to ' + options.contentBase.target);
+        else
+            console.log('content is served from ' + options.contentBase);
+        if (options.historyApiFallback)
+            console.log('404s will fallback to %s', options.historyApiFallback.index || '/index.html');
+    });
+
 }
 
 function helpInfo() {
@@ -156,7 +299,7 @@ function ifArg(name, fn, init) {
     if (Array.isArray(argv[name])) {
         if (init) init();
         argv[name].forEach(fn);
-    } else if (typeof argv[name] !== "undefined") {
+    } else if (typeof argv[name] !== 'undefined') {
         if (init) init();
         fn(argv[name], -1);
     }
